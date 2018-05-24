7e7fd9465d0239c922a28eee47f17713667d1a00
haizatulaziah <haizatulaziah@gmail.com>
2018-05-24 10:09:23 +0800

    Family Planning

---
+++ findHighandLower.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Find Largest & Smallest numbers in an Array - PHP Programming</title>
+</head>
+<body>
+	
+	<h3>PHP Program to find Largest & Smallest numbers in a Array by <a href="tutorialsmade.com">Tutorialsmade.com</a></h3>
+	Enter the Numbers separated by Commas <br />
+	(eg: 12,23,56)
+	<br /><br />
+	<form method="post" action="findHighandLower2.php">
+		<input type="text" name="numbers"/>
+		<button type="submit">Check</button>
+	</form>
+</body>
+</html>
+ 
